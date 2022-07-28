ea230595a8160207605d3cf42d5ff6314f424d8a
newaritra <aritraroy14.ar@gmail.com>
2022-07-29 00:20:40 +0530

    github pages dependency added and styling

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 import { db } from "../data/db";
 const Landing: React.FC = () => {
   return (
-    <div className="h-screen w-screen bg-landing-bg overflow-auto">
+    <div className="landing h-screen w-screen bg-landing-bg overflow-auto">
       <div className="trending-assets-container flex mt-56 ml-20 gap-4 items-center">
         <img className="w-4 h-4" src="./activity.png" alt="trending" />
         <p className="text-base font-semibold text-white">TRENDING ASSETS</p>
